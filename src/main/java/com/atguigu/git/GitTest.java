f1fe76b16fa1089afa7a3d100f77ebe912bed263
_oah <782773467@qq.com>
2023-01-12 21:54:32 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName GitTest
+ * @Description TODO
+ * @Author _oah
+ * @Date 2023.1.12 21:46
+ * @Version 1.0
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
